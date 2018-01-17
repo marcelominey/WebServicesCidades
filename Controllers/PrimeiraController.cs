aa40ab09018cf3bb37a1c2383a0c125550a9f6b5
MARCELO MINEY MENDES <22386773809@jpmorgan.sp>
2018-01-16 22:01:48 -0200

    first commit

---
+++ Controllers/PrimeiraController.cs
@@ -0,0 +1,32 @@
+using System;
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+using WebServicesCidades.Models;
+
+namespace WebServicesCidades.Controllers
+{
+    //Vamos definir aqui a rota para a requisição do serviço
+    [Route("api/[controller]")]
+    public class PrimeiraController:Controller
+    {
+        Cidades cidade = new Cidades();
+
+        /*[HttpGet] //vou requisitar o serviço, mas também estou passando um parâmetro (id)
+        public IEnumerable<string> Get(){
+            return new string[] {"Curitiba", "Porto Alegre", "Salvador", "Belo Horizonte"};
+            //Ienumerable no MVC: ler o conteúdo de uma lista, no View.
+            //Ienumerable aqui: não temos View, mostraremos após a requisição, num formato de leitura, JSON.
+        }*/
+        
+        /*[HttpGet("{id}")] //vou requisitar o serviço, mas também estou passando um parâmetro (id)
+        public string Get(int id){
+            return new string[] {"Curitiba", "Porto Alegre", "Salvador", "Belo Horizonte"}[id];
+        }*/
+                
+        [HttpGet]
+        public IEnumerable<Cidades> Get(){
+            return cidade.Listar();
+            //vai retornar todas as "cidades" em uma lista
+        }
+    }
+}
---
+++ Startup.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace WebServicesCidades
+{
+    public class Startup
+    {
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc();
+            //Adicionando o Serviço de MVC para utilizar Rotas e Controlador
+            //Controller
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment()) //se o ambiente for de desenvolvimento:
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseMvc();
+
+            app.Run(async (context) =>
+            {
+                await context.Response.WriteAsync("Hello World!");
+            });
+        }
+    }
+}
