7bc7295893050d3f98b5e8f7606c382a7e43a81e
Marcelo Miney <32588950+marcelominey@users.noreply.github.com>
2018-01-18 17:33:53 +0000

    Update DAOCidades.cs

--- Models/DAOCidades.cs
+++ Models/DAOCidades.cs
@@ -1,3 +1,4 @@
+//esse arquivo foi clonado do projeto da Naila
 using System.Data.SqlClient;
         using System.Data;
         using System.Collections.Generic;
@@ -11,7 +12,6 @@ using System.Data.SqlClient;
                 SqlCommand cmd = null;
                 SqlDataReader rd = null;
       
-        
                 string conexao = @"Data Source=.\SqlExpress;Initial Catalog=ProjetoCidades;user id=sa;password=senai@123";// ProjetoCidades nome do BD.
         
                 public List<Cidades> Listar()
@@ -77,275 +77,72 @@ using System.Data.SqlClient;
                     }
                     catch(Exception ex){
                         throw new Exception(ex.Message);
-      
-      
-        
+          
                     }
-      
-      
-        
                     finally{
-      
-      
-        
                         con.Close();
-      
-      
-        
                     }
-      
-      
-        
                     return resultado;
-      
-      
-        
                 }
-      
-      
-        
                     public bool Atualizar(Cidades cidades){
-      
-      
-        
                     bool resultado = false;
-      
-      
-        
                     try {
-      
-      
-        
                         con = new SqlConnection(conexao);
-      
-      
-        
                         con.Open();
-      
-      
-        
                         cmd = new SqlCommand();
-      
-      
-        
                         cmd.Connection = con;
-      
-      
-        
                         cmd.CommandType = CommandType.Text;
-      
-      
-        
                         cmd.CommandText = "UPDATE cidades set nome=@n, estado=@e, habitantes=@h where id=@id"; // cidades é o nome da tabela
-      
-      
-        
                         cmd.Parameters.AddWithValue("@n",cidades.Nome);
-      
-      
-        
                         cmd.Parameters.AddWithValue("@e",cidades.Estado);
-      
-      
-        
                         cmd.Parameters.AddWithValue("@h",cidades.Habitantes);
-      
-      
-        
                         cmd.Parameters.AddWithValue("@id",cidades.Id);
-      
-      
-        
-        
-
-      
-      
         
                         int r = cmd.ExecuteNonQuery();
-      
-      
-        
                         if(r > 0)
-      
-      
-        
                         resultado = true;
-      
-      
         
-        
-
-      
-      
         
                         cmd.Parameters.Clear();
-      
-      
-        
                     }
-      
-      
-        
                     catch(SqlException se){
-      
-      
-        
                         throw new Exception(se.Message);
-      
-      
-        
                     }
-      
-      
-        
                     catch(Exception ex){
-      
-      
-        
                         throw new Exception(ex.Message);
-      
-      
-        
                     }
-      
-      
-        
                     finally{
-      
-      
-        
                         con.Close();
-      
-      
-        
                     }
-      
-      
-        
                     return resultado;
-      
-      
-        
                 }
-      
-      
-        
                 public bool Deletar(int id){
-      
-      
-        
                     bool resultado = false;
-      
-      
-        
                     try {
-      
-      
-        
                         con = new SqlConnection(conexao);
-      
-      
-        
                         con.Open();
-      
-      
-        
                         cmd = new SqlCommand();
-      
-      
-        
                         cmd.Connection = con;
-      
-      
-        
                         cmd.CommandType = CommandType.Text;
-      
-      
-        
                         cmd.CommandText = "DELETE FROM cidades WHERE id=@id";
-      
-      
-        
                         cmd.Parameters.AddWithValue("@id",id);
-      
-      
-        
-        
-
-      
-      
         
                         int r = cmd.ExecuteNonQuery();
-      
-      
-        
                         if(r > 0)
-      
-      
-        
                         resultado = true;
-      
-      
-        
-        
-
-      
-      
         
                         cmd.Parameters.Clear();
-      
-      
-        
                     }
-      
-      
-        
                     catch(SqlException se){
-      
-      
-        
                         throw new Exception(se.Message);
-      
-      
-        
                     }
-      
-      
-        
                     catch(Exception ex){
-      
-      
-        
                         throw new Exception(ex.Message);
-      
-      
-        
                     }
-      
-      
-        
                     finally{
-      
-      
-        
                         con.Close();
-      
-      
-        
                     }
-      
-      
-        
                     return resultado;
-      
-      
-        
                 }
-      
-      
-        
             }
-      
-      
-        
         }
