eedca9c279b5f6a74ee9fe2a5a7b42fbaaa0fde9
Marcelo Miney <32588950+marcelominey@users.noreply.github.com>
2018-01-18 17:52:12 +0000

    Atualizando para incluir DAOCidades.cs

--- Controllers/PrimeiraController.cs
+++ Controllers/PrimeiraController.cs
@@ -25,10 +25,23 @@ namespace WebServicesCidades.Controllers
             return new string[] {"Curitiba", "Porto Alegre", "Salvador", "Belo Horizonte"}[id];
         }*/
                 
-        [HttpGet]
+        /*[HttpGet]
         public IEnumerable<Cidades> Get(){
             return cidade.Listar();
             //vai retornar todas as "cidades" em uma lista
+        }*/
+        [HttpGet]        
+        public IEnumerable<Cidades> Get(){
+            return dao.Listar();
         }
+        [HttpGet("{id}")]
+        public Cidades Get(int id){
+            return dao.Listar().Where(x => x.Id == id).FirstOrDefault();
+        }
+        [HttpPost]
+        public IActionResult Post([FromBody]Cidades cidades){ //cidades vem do corp do front end
+            dao.Cadastrar(cidades);
+            return CreatedAtRoute("CidadeAtual", new{id=cidades.Id},cidades);
+        }
     }
 }
